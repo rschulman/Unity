a7f5db62e21e196ae9a158087f904137cce9f4c7
Ross Schulman <rschulman@gmail.com>
2012-01-04 13:23:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unity::Application.config.secret_token = '28d235925476cc061e8f22b785678c16fbfcb6064fe935d18bc5bb1e802d1f4a12072972f9ea97c679f827bcc834def3a2c3be7b3e4310e27572f35a9bfb268a'
